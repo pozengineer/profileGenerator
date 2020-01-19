f6dafe5e8b579d4440857bd9e70cd9461b2316e8
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-01-19 18:15:31 +1100

    stuck on index.js line 47 generateHTML not a function

--- generateHTML.js
+++ generateHTML.js
@@ -25,9 +25,9 @@ const colors = {
   }
 };
 
-function generateHTML(data) {
+const generateHTML = data => {
   return `<!DOCTYPE html>
-<html lang="en">
+  <html lang="en">
    <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -171,4 +171,8 @@ function generateHTML(data) {
           } 
          }
       </style>`
-        }
+}
+
+module.exports = {
+  generateHTML: generateHTML
+};
--- index.js
+++ index.js
@@ -1,11 +1,69 @@
-const questions = [
-  
-];
+// const questions = [
 
-function writeToFile(fileName, data) {
- 
-}
+// ];
+// function writeToFile(fileName, data) {
 
-function init() {
+// }
+// function init() {
 
-init();
+// }
+// init();
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require('./generateHTML.js');
+const axios = require('axios');
+const convertFactory = require('electron-html-to');
+
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your gitHub username?',
+        name: 'userName',
+    },
+    {
+        type: 'list',
+        message: 'What is your preferred colour?',
+        name: 'userColor',
+        choices: [
+            "green",
+            "blue",
+            "pink",
+            'red'
+        ]
+    }
+])
+    .then((data) => {
+        const userName = data.userName;
+        const userColor = data.userColor;
+        console.log(userName);
+        console.log(userColor);
+        const queryUrl = `https://api.github.com/users/${userName}`;
+
+        axios.get(queryUrl).catch().then(response => {
+            console.log(response.data);
+            console.log(userColor);
+            return generateHTML({ userColor, ...response.data});
+        })
+        .then(htmlData => {
+            console.log(htmlData);
+        });
+    });
+
+//     .then((htmlData) => {
+//         // console.log(htmlData)
+//         var conversion = convertFactory({
+//             converterPath: convertFactory.converters.PDF
+//             });
+//             conversion({ html: htmlData }, function(err, result) {
+//             if (err) {
+//                 return console.error(err);
+//             }
+//             console.log(result.numberOfPages);
+//             // console.log(result.logs);
+//             result.stream.pipe(fs.createWriteStream('tonPDF.pdf'));
+//             conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+//             });
+//     })
+// });
